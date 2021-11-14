fba59af896ded058dd3494383eed1abedcf314c0
chengguan <chengguan@gaoding.com>
2021-11-14 15:53:47 +0800

    Complete Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,21 +7,32 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("神秘数字是：{}", secret_number);
+    // println!("神秘数字是：{}", secret_number);
 
-    println!("猜一个数");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("无法读取行!");
-
-    println!("你猜测的数是: {}", guess);
-
-    let guess: u32 = guess.trim().parse().expect("请输入一个数字");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("太小啦！"),
-        Ordering::Equal => println!("对啦！"),
-        Ordering::Greater => println!("太大啦！"),
+    loop {
+        println!("猜一个数");
+    
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).expect("无法读取行!");
+    
+        println!("你猜测的数是: {}", guess);
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("请输入数字");
+                continue;
+            }
+        };
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小啦！"),
+            Ordering::Greater => println!("太大啦！"),
+            Ordering::Equal => {
+                println!("对啦！");
+                break;
+            },
+        }
     }
 }
