58963de23296db2d78f27479ed2f57d706b5db8c
chengguan <chengguan@gaoding.com>
2021-11-14 15:21:11 +0800

    rand

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("猜数");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("神秘数字是：{}", secret_number);
 
     println!("猜一个数");
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("无法读取行");
+    io::stdin().read_line(&mut guess).expect("无法读取行!");
 
     println!("你猜测的数是: {}", guess);
 }
