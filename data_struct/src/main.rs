de337b0491b0cb55e4ceb177036943f583b0da42
chengguan <chengguan@gaoding.com>
2021-11-20 16:10:37 +0800

    chore

--- data_struct/src/main.rs
+++ data_struct/src/main.rs
@@ -9,6 +9,9 @@ impl Rectangle {
     }
 }
 
+
+
+
 fn main() {
     println!("Hello, world!");
 
@@ -18,4 +21,36 @@ fn main() {
     };
 
     println!("Rect width is {}, height is {}, area is {}", rect1.width, rect1.height, rect1.area());
+
+
+    struct User {
+        name: String,
+        age: i32,
+        email: String,
+        active: bool,
+    }
+
+    impl User {
+        fn hi(&self) -> String {
+            let mut hi = String::from("Hi, ");
+            let name: &str = &self.name[..];
+            hi.push_str(name);
+            return hi;
+        }
+    }
+
+    
+    let lily = User {
+        name: String::from("Lily"),
+        age: 12,
+        active: true,
+        email: String::from("lily@qq.com"),
+    };
+
+    let daer = User {
+        name: String::from("daer"),
+        ..lily
+    };
+
+    println!("{}", daer.hi());
 }
